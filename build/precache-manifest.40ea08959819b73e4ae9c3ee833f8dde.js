49c64d58149d56286722b51313065fc8337cc3ce
Soraya Sacaan <ssacaan@aaxiscommerce.com>
2020-09-18 13:06:46 -0300

    yarn home page

---
+++ build/precache-manifest.40ea08959819b73e4ae9c3ee833f8dde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251f49a338b1747ea7415282d029c2c9",
+    "url": "/fed-challenge/index.html"
+  },
+  {
+    "revision": "d1f7af916bfa5a541b24",
+    "url": "/fed-challenge/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "29ed372c24e4ca1b70b3",
+    "url": "/fed-challenge/static/css/main.7cec003b.chunk.css"
+  },
+  {
+    "revision": "d1f7af916bfa5a541b24",
+    "url": "/fed-challenge/static/js/2.38477a30.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fed-challenge/static/js/2.38477a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ed372c24e4ca1b70b3",
+    "url": "/fed-challenge/static/js/main.082be43d.chunk.js"
+  },
+  {
+    "revision": "67446a3856a65f5d648b",
+    "url": "/fed-challenge/static/js/runtime-main.95562028.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fed-challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de296ce8b01ce2270b4ab0a5a034df88.js"
+  "/fed-challenge/precache-manifest.40ea08959819b73e4ae9c3ee833f8dde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fed-challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
