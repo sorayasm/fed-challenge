bd182383aeb14050165513f14c23889a068e99f3
Soraya Sacaan <ssacaan@aaxiscommerce.com>
2020-09-18 12:42:20 -0300

    Updates

---
+++ precache-manifest.4439f12a5ea10e8875707c57a1eb20e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957d62bad2396e969ca55789d5bd2cde",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a8832f8a7eaef2e50f08",
+    "url": "/react-gh-pages/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "118b1f3b2bcdbdff536e",
+    "url": "/react-gh-pages/static/css/main.7cec003b.chunk.css"
+  },
+  {
+    "revision": "a8832f8a7eaef2e50f08",
+    "url": "/react-gh-pages/static/js/2.38477a30.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-gh-pages/static/js/2.38477a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118b1f3b2bcdbdff536e",
+    "url": "/react-gh-pages/static/js/main.641ed89c.chunk.js"
+  },
+  {
+    "revision": "402c23767916cae8dbb4",
+    "url": "/react-gh-pages/static/js/runtime-main.180d5efa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
